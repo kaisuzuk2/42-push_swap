dc2905b199c3ec9356c8e599374b06db3b821a5e
Kaisei Suzuki <kaisuzuk@c3r4s1.42tokyo.jp>
2025-05-28 21:30:39 +0900

    ソートできた

--- cost_utils.c
+++ cost_utils.c
@@ -5,6 +5,16 @@ int	get_total_cost(t_cost *cst)
 	return (cst->ra + cst->rra + cst->rb + cst->rrb + cst->rr + cst->rrr);
 }
 
+int	get_forward_cost(t_cost *cst)
+{
+	return (cst->ra + cst->rb + cst->rr);
+}
+
+int	get_backward_cost(t_cost *cst)
+{
+	return (cst->rra + cst->rrb + cst->rrr);
+}
+
 t_cost	*cost_init(t_cost *cst)
 {
 	cst->ra = 0;
@@ -19,29 +29,41 @@ t_cost	*cost_init(t_cost *cst)
 
 t_cost	*optimize_common_rotation(t_cost *cst)
 {
-	if (cst->ra > 0 && cst->rb > 0)
+	while (cst->ra > 0 && cst->rb > 0)
 	{
 		cst->rr++;
 		cst->ra--;
 		cst->rb--;
 	}
-	if (cst->rra > 0 && cst->rrb > 0)
+	while (cst->rra > 0 && cst->rrb > 0)
 	{
 		cst->rrr++;
 		cst->rra--;
 		cst->rrb--;
 	}
+	if (get_forward_cost(cst) <= get_backward_cost(cst))
+	{
+		cst->rrr = 0;
+		cst->rra = 0;
+		cst->rrb = 0;
+	}
+	else
+	{
+		cst->rr = 0;
+		cst->ra = 0;
+		cst->rb = 0;
+	}
 	return (cst);
 }
 
-t_cost	*stack_b_cost(t_cost *cst, t_stack stk_b, int rank)
+t_cost	*stack_b_cost(t_cost *cst, t_stack *stk_b, int rank)
 {
 	int		i;
 	t_stack	*cur;
 
 	i = 0;
 	cur = stk_b->next;
-	while (cur != -stk_b)
+	while (cur != stk_b)
 	{
 		if (cur->rank < rank && rank < cur->next->rank)
 			break ;
@@ -50,11 +72,13 @@ t_cost	*stack_b_cost(t_cost *cst, t_stack stk_b, int rank)
 	}
 	cst->rb = i;
 	cst->rrb = stk_b->size - i;
+	return (cst);
 }
 
 t_cost	*calc_cost(t_stack *stk_a, t_stack *stk_b)
 {
 	t_cost *best_cst;
+	t_cost *cur_cst;
 	t_stack *cur;
 	int idx;
 
@@ -69,11 +93,11 @@ t_cost	*calc_cost(t_stack *stk_a, t_stack *stk_b)
 		cur_cst->rra = stk_a->size - idx;
 		stack_b_cost(cur_cst, stk_b, cur->rank);
 		optimize_common_rotation(cur_cst);
-		if (!best_cst || get_total_cost(cur_cos) < get_total_cost(best_cst))
+		if (!best_cst || get_total_cost(cur_cst) < get_total_cost(best_cst))
 		{
 			if (best_cst)
 				free(best_cst);
-			best_cost = cur_cst;
+			best_cst = cur_cst;
 		}
 		else
 			free(cur_cst);
---
+++ do_sort.c
@@ -0,0 +1,49 @@
+#include "push_swap.h"
+
+void do_rotate_both(t_cost *cst, t_stack *stk_a, t_stack *stk_b)
+{
+	while (cst->rr)
+	{
+		rotate(stk_a);
+		rotate(stk_b);
+		cst->rr--;
+	}
+	while (cst->rrr)
+	{
+		reverse_rotate(stk_a);
+		reverse_rotate(stk_b);
+		cst->rrr--;
+	}
+}
+
+void do_rotate(t_cost *cst, t_stack *stk_a, t_stack *stk_b)
+{
+	while (cst->ra)
+	{
+		rotate(stk_a);
+		cst->ra--;
+	}
+	while (cst->rb)
+	{
+		rotate(stk_b);
+		cst->rb--;
+	}
+	while (cst->rra)
+	{
+		reverse_rotate(stk_a);
+		cst->rra--;
+	}
+	while (cst->rrb)
+	{
+		reverse_rotate(stk_b);
+		cst->rrb--;
+	}
+}
+
+void	do_sort(t_cost *cst, t_stack *stk_a, t_stack *stk_b)
+{
+	do_rotate_both(cst, stk_a, stk_b);
+	do_rotate(cst, stk_a, stk_b);
+	push(stk_b, stk_a);
+	free(cst);
+}
